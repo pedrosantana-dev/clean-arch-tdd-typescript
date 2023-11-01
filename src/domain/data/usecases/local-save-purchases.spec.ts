2e6f19859b7338c61fad8e090a08ae945c8b80ea
Pedro Santana <pedromanu27@gmail.com>
2023-10-31 22:52:28 -0300

    mesclagem de teste

--- src/domain/data/usecases/local-save-purchases.spec.ts
+++ src/domain/data/usecases/local-save-purchases.spec.ts
@@ -39,15 +39,10 @@ describe('LocalSavePurchases', () => {
 		expect(cacheStore.deleteCallsCount).toBe(0);
 	});
 
-	test('Should delete old cache on sut.save', async () => {
-		const { cacheStore, sut } = makeSut();
-		await sut.save();
-		expect(cacheStore.deleteCallsCount).toBe(1);
-	});
-
 	test('Should call delete with correct key', async () => {
 		const { cacheStore, sut } = makeSut();
 		await sut.save();
 		expect(cacheStore.key).toBe('purchases');
+		expect(cacheStore.deleteCallsCount).toBe(1);
 	});
 });
