b191c46aeb91a443acc415a08f6cc9c927198d77
Pedro Santana <pedromanu27@gmail.com>
2023-11-02 17:19:56 -0300

    garante que LocalLoadPurchases chama cache store com key correta

--- src/data/usecases/load-purchases/local-load-purchases.ts
+++ src/data/usecases/load-purchases/local-load-purchases.ts
@@ -2,12 +2,17 @@ import { CacheStore } from '@/data/contracts/cache';
 import { SavePurchases } from '@/domain/usecases';
 
 export class LocalLoadPurchases implements SavePurchases {
+	private readonly key = 'purchases';
 	constructor(private readonly chacheStore: CacheStore, private readonly timestamp: Date) {}
 
 	async save(purchases: Array<SavePurchases.Params>): Promise<void> {
-		this.chacheStore.replace('purchases', {
+		this.chacheStore.replace(this.key, {
 			timestamp: this.timestamp,
 			value: purchases,
 		});
+	}
+
+	async loadAll(): Promise<void> {
+		this.chacheStore.fetch(this.key);
 	}
 }
