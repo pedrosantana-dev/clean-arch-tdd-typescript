11ed7767029f54a33e5f356a90fd5c0b8676b20c
Pedro Santana <pedromanu27@gmail.com>
2023-11-02 15:59:03 -0300

    simplifica CacheStore insertion com um metodo replace

--- src/data/usecases/save-purchases/local-save-purchases.ts
+++ src/data/usecases/save-purchases/local-save-purchases.ts
@@ -5,8 +5,7 @@ export class LocalSavePurchases implements SavePurchases {
 	constructor(private readonly chacheStore: CacheStore, private readonly timestamp: Date) {}
 
 	async save(purchases: Array<SavePurchases.Params>): Promise<void> {
-		this.chacheStore.delete('purchases');
-		this.chacheStore.insert('purchases', {
+		this.chacheStore.replace('purchases', {
 			timestamp: this.timestamp,
 			value: purchases,
 		});
