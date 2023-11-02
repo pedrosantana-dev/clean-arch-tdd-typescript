bbfe0b0b5b99c078b41de0870e6178e3a7481977
Pedro Santana <pedromanu27@gmail.com>
2023-11-02 15:41:27 -0300

    mescla testes

--- src/data/usecases/save-purchases/local-save-purchases.spec.ts
+++ src/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -1,5 +1,5 @@
-import { LocalSavePurchases } from "@/data/usecases";
-import { mockPurchases, CacheStoreSpy } from "@/data/tests";
+import { LocalSavePurchases } from '@/data/usecases';
+import { mockPurchases, CacheStoreSpy } from '@/data/tests';
 
 type SutTypes = {
 	sut: LocalSavePurchases;
@@ -15,20 +15,13 @@ const makeSut = (): SutTypes => {
 	};
 };
 
-describe("LocalSavePurchases", () => {
-	test("Should not delete or insert cache on sut.init", () => {
+describe('LocalSavePurchases', () => {
+	test('Should not delete or insert cache on sut.init', () => {
 		const { cacheStore } = makeSut();
 		expect(cacheStore.messages).toEqual([]);
 	});
 
-	test("Should delete old cache on sut.save", async () => {
-		const { cacheStore, sut } = makeSut();
-		const purchases = mockPurchases();
-		await sut.save(purchases);
-		expect(cacheStore.messages).toEqual([CacheStoreSpy.Message.delete, CacheStoreSpy.Message.insert]);
-	});
-
-	test("Should not insert new Cache if delete fails", async () => {
+	test('Should not insert new Cache if delete fails', async () => {
 		const { cacheStore, sut } = makeSut();
 		cacheStore.simulateDeleteError();
 		// try {
@@ -41,16 +34,17 @@ describe("LocalSavePurchases", () => {
 		expect(cacheStore.messages).toEqual([CacheStoreSpy.Message.delete]);
 	});
 
-	test("Should insert new Cache if delete succeeds", async () => {
+	test('Should insert new Cache if delete succeeds', async () => {
 		const { cacheStore, sut } = makeSut();
 		const purchases = mockPurchases();
 		await sut.save(purchases);
 		expect(cacheStore.messages).toEqual([CacheStoreSpy.Message.delete, CacheStoreSpy.Message.insert]);
-		expect(cacheStore.insertKey).toBe("purchases");
+		expect(cacheStore.deleteKey).toBe('purchases');
+		expect(cacheStore.insertKey).toBe('purchases');
 		expect(cacheStore.insertValues).toEqual(purchases);
 	});
 
-	test("Should throw if insert throws", async () => {
+	test('Should throw if insert throws', async () => {
 		const { cacheStore, sut } = makeSut();
 		cacheStore.simulateInsertError();
 		await expect(sut.save(mockPurchases())).rejects.toThrow();
